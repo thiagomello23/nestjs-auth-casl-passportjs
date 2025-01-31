94b05c1eb08c63d000ec8ddcd5f53f18e4b11f30
thiago <thiagomellodiniz@hotmail.com>
2025-01-31 16:39:45 -0400

    fix: adding validation pipe for class validator works;

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SeedService } from './seed/seed.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
     const seed = app.get(SeedService)
     await seed.seed()
   }
+
+  app.useGlobalPipes(new ValidationPipe())
 
   await app.listen(process.env.PORT ?? 3000);
 }
